56ae669a958ea2975dc089efdc24f893c011c7bf
Hu Yifei <ascalon0823@gmail.com>
2016-09-28 13:16:15 +0800

    Implement test initializer for game class

--- game.rb
+++ game.rb
@@ -1,6 +1,16 @@
+require_relative "attribute.rb"
+
 class Game
     @@gameVersion = 0.01
     def self.version
-        puts @@gameVersion
+        puts "Current game version = #@@gameVersion"
     end
+    
+    def initialize
+        Attribute.load
+        Attribute.print
+    end
+
+    
+
 end
--- main.rb
+++ main.rb
@@ -1,3 +1,6 @@
 require_relative "game.rb"
+require_relative "engine.rb"
+
 puts "Welcome to SpellCraft"
 Game.version
+Game.new
