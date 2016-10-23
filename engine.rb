32d4bc749a4a6bfa6e52ee5e9cc89d350489c937
Ascalon0823 <ascalon0823@gmail.com>
2016-10-23 16:24:00 +0800

    Change class variable from array to hash

--- engine.rb
+++ engine.rb
@@ -3,7 +3,7 @@ require_relative "attribute.rb"
 class Engine
     def self.add_attr(name,type,value)
         Attribute.load
-        attr = Attribute.new(name,type,value)
+        Attribute.new(name,type,value)
         Attribute.save
     end
     
