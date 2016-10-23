625250857c0af992be8b4c2d25c484a999ad4301
bleac <bleac@172.23.178.46>
2016-10-23 09:52:23 +0800

    Implement fundamental function for Character class: Loading attributes; character creation.

--- character.rb
+++ character.rb
@@ -1,5 +1,29 @@
 require "attribute.rb"
 class Character
-    @@characters = []
-    @@no_of_characters = 0
+     @@characters = []
+     @@no_of_characters = 0
+     @@ha_charAttr = Hash.new
+    
+  def self.load
+      @@ha_charAttr.clear
+      @@characters = []
+      @@no_of_character = 0
+      if(Attribute.no_of_attributes == 0)
+          Attribute.load
+      end
+      Attribute.attributes.each{ |attr|
+          if ( attr.getType == "Character")
+               @@ha_charAttr[attr.getName] = attr
+          end
+     }
+  end
+  
+  def initialize(name)
+     @name = name
+     @ha_attr = Hash.new
+     @possesion = []
+     @@ha_charAttr.each { |key,attr|  @ha_attr[key]=attr.getValue}
+     @@no_of_characters +=1
+     @@characters<<self
+  end
 end
